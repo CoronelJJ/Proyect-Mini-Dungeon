83d60dcec4e41abe84ff552f2ea5545a09f2fab3
CoronelJJ <jjcoronel104@gmail.com>
2025-09-19 16:17:03 -0300

    player movement animations

--- Assets/Scenes/Scripts/PlayerMovement.cs
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -1,25 +1,55 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float movementSpeed = 10.0f;
+    public float movementSpeed = 5.0f;
     private Rigidbody2D rb;
+
+    [SerializeField] Animator animator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector2 movVector = new Vector2(horizontalInput, verticalInput);
-        movVector.Normalize();
+        Vector2 movVector = setMoveVector(horizontalInput, verticalInput);
+
+        animator.SetFloat("movementX", movVector.x);
+        animator.SetFloat("movementY", movVector.y);
+
+        Debug.Log(movVector);
 
         rb.linearVelocity = movVector * movementSpeed;
 
+    }
+
+
+    Vector2 setMoveVector(float horizontalInput, float verticalInput)
+    {
+        Vector2 movVector = new Vector2();
+
+        if (horizontalInput != 0)
+        {
+            movVector.x = horizontalInput;
+            movVector.y = 0;
+            movVector.Normalize();
+
+        }
+        else if (verticalInput != 0)
+        {
+            movVector.x = 0;
+            movVector.y = verticalInput;
+            movVector.Normalize();
+
+        }
+
+        return movVector;
     }
 }
